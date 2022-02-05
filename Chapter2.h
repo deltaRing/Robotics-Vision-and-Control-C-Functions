a36c2b8731a498fe52cb105e6452ff344bad738f
Laoxiang-Git <43900243+Laoxiang-Git@users.noreply.github.com>
2022-02-05 14:29:43 +0800

    Create Chapter2.h
    第二章用到的函数头文件

---
+++ Chapter2.h
@@ -0,0 +1,14 @@
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <iostream>
+#include <cmath>
+
+using namespace Eigen;
+using namespace std;
+
+Vector2d RotateAndPosition(double Theta, Vector2d PositionInB, Vector2d Position);
+Matrix3d SE2(double x, double y, double Theta); // 相对位姿求解
+Matrix3d rotx(double Theta);
+Matrix3d roty(double Theta);
+Matrix3d rotz(double Theta);
+Vector3d tr2eul(Matrix3d R);
