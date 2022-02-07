b88d0baa5231d8903e3b8ee1ef66e9d938d255f1
e1ysium <43900243+deltaRing@users.noreply.github.com>
2022-02-07 16:04:49 +0800

    更新多段轨迹

--- Chapter3.h
+++ Chapter3.h
@@ -1,4 +1,5 @@
 #include "Chapter2.h"
+#include <vector>
 
 struct TPoly {
 	VectorXd s; // 生成多项式轨迹 
@@ -9,6 +10,27 @@ struct TPoly {
 	ArrayXXd Sd; // 速度
 	ArrayXXd Sdd; // 加速度
 };
+
+double ceil(double tmp) {
+	if (tmp > 0)
+		return tmp - int(tmp) > 0 ? int(tmp) + 1 : int(tmp);
+	else if (tmp < 0)
+		return int(tmp);
+	return 0;
+}
+
+ArrayXd ceil(ArrayXd tmp) {
+	ArrayXd ttt(tmp.size());
+	for (int iii = 0; iii < tmp.size(); iii++) {
+		if (tmp(iii) > 0)
+			ttt(iii) = tmp(iii) - int(tmp(iii)) > 0 ? int(tmp(iii)) + 1 : int(tmp(iii));
+		else if (tmp(iii) < 0)
+			ttt(iii) = int(tmp(iii));
+		else
+			ttt(iii) = 0;
+	}
+	return ttt;
+}
 
 // p(x) = 3x ^ 2 + 2x + 1 at x = 5, 7, and 9:
 // p = [3 2 1];
@@ -256,4 +278,173 @@ TPoly mtraj(TPoly(*func)(double, double, double), VectorXd q0, VectorXd qf, doub
 	_res_.Sd = sd;
 	_res_.Sdd = sdd;
 	return _res_;
+}
+
+// Compute a joint space trajectory
+ArrayXXd jtraj(ArrayXd q0, ArrayXd q1, ArrayXd tv, ArrayXd qd0, ArrayXd qd1) {
+	ArrayXd t;
+	double tscal;
+	if (tv.size() > 1) {
+		tscal = tv.maxCoeff();
+		t = tv / tscal;
+	}
+	else {
+		tscal = 1;
+		t = ArrayXd::LinSpaced(int(tv(0)), 0, tv(0) - 1);
+	}
+
+	// compute the polynomial coefficients
+	ArrayXd A = 6 * (q1 - q0) - 3 * (qd1 + qd0)*tscal;
+	ArrayXd B = -15 * (q1 - q0) + (8 * qd0 + 7 * qd1)*tscal;
+	ArrayXd C = 10 * (q1 - q0) - (6 * qd0 + 4 * qd1)*tscal;
+	ArrayXd E = qd0 * tscal; // as the t vector has been normalized
+	ArrayXd F = q0;
+
+	ArrayXXd tt(t.size(), 6);
+	tt.col(0) = pow(t, 5);
+	tt.col(1) = pow(t, 4);
+	tt.col(2) = pow(t, 3);
+	tt.col(3) = pow(t, 2);
+	tt.col(4) = pow(t, 1);
+	tt.col(5) = pow(t, 0);
+
+	ArrayXXd c(6, A.size());
+	c.row(0) = A;
+	c.row(1) = B;
+	c.row(2) = C;
+	c.row(3) = ArrayXd::Zero(A.size());
+	c.row(4) = E;
+	c.row(5) = F;
+
+	ArrayXXd _res_ = tt.matrix() * c.matrix();
+	return _res_;
+}
+
+// MSTRAJ Multi-segment multi-axis trajectory
+ArrayXXd mstraj(ArrayXXd segments, ArrayXd qdmax, ArrayXd tsegment, ArrayXd q0, double dt, ArrayXd Tacc) {
+	ArrayXXd R;
+	if (q0.size() == 0) {
+		q0 = segments.row(0);
+		segments = segments.block(1, 0, segments.rows() - 1, segments.cols());
+	}
+	
+	if (qdmax.size() > 0 && tsegment.size() > 0) {
+		cout << "Error: Must Specify qdMax or tsegment but not both" << endl;
+		return R;
+	}
+
+	if (segments.cols() != q0.size()) {
+		cout << "Error: WP and Q0 must have same number of columns" << endl;
+		return R;
+	}
+
+	if (qdmax.size() == 0) {
+		if (tsegment.size() != segments.rows()) {
+			cout << "Error: Length of TSEG does not match number of segments" << endl;
+			return R;
+		}
+	}
+
+	if (tsegment.size() == 0) {
+		if (qdmax.size() == 1) {
+			double temp = qdmax(0);
+			qdmax = ArrayXd::Ones(segments.cols());
+			qdmax = qdmax * temp;
+		}
+		if (qdmax.size() != segments.cols()) {
+			cout << "Length of QDMAX does not match number of axes" << endl;
+			return R;
+		}
+	}
+
+	int ns = segments.rows();
+	int nj = segments.cols();
+
+	ArrayXd qd0 = ArrayXd::Zero(nj);
+	ArrayXd qdf = ArrayXd::Zero(nj);
+
+	ArrayXd q_prev = q0;
+	ArrayXd qd_prev = qd0;
+	double clock = 0.0;
+	std::vector <double> arrive;
+	std::vector <ArrayXd> tg;
+	ArrayXd q_next; 
+	double tacc2;
+	double taccx;
+	ArrayXXd qb;
+
+	for (int seg = 0; seg < ns; seg++) {
+		double tacc;
+		if (Tacc.size() > 1) {
+			tacc = Tacc(seg);
+		}
+		else {
+			tacc = Tacc(0);
+		}
+		tacc = ceil(tacc / dt) * dt;
+		tacc2 = ceil(tacc / 2.0 / dt) * dt;
+		double taccx;
+		if (seg == 0) {
+			taccx = tacc2;
+		}
+		else {
+			taccx = tacc;
+		}
+		
+		q_next = segments.row(seg);
+		ArrayXd dq = q_next - q_prev;
+		ArrayXXd qb;
+		double tseg;
+		int slowest;
+
+		if (qdmax.size() != 0) {
+			qb = taccx * qdmax / 2.0;
+			double tb = taccx;
+
+			ArrayXd tl = abs(dq) / qdmax;
+			tl = ceil(tl / dt) * dt;
+
+			ArrayXd tt = tb + tl;
+			tseg = tt.maxCoeff(&slowest);
+			if (tseg <= 2 * tacc)
+				tseg = 2 * tacc;
+		}
+		else if (tsegment.size() > 0) {
+			tseg = tsegment(seg);
+			slowest = NAN;
+		}
+		arrive.push_back(clock + tseg);
+		if (seg > 0)
+			arrive[seg] += tacc2;
+
+		ArrayXd qd = dq / tseg;
+		qb = jtraj(q0, q_prev + tacc2 * qd, ArrayXd::LinSpaced(int(taccx / dt), 0, taccx), qd_prev, qd);
+
+		for (int ii = 0; ii < qb.rows() - 1; ii++) {
+			tg.push_back(qb.row(ii + 1));
+		}
+		clock = clock + taccx;
+		// add the linear part, from tacc/2+dt to tseg-tacc/2
+		for (double t = tacc2 + dt; t < tseg - tacc2; t += dt) {
+			double s = t / tseg;
+			q0 = (1 - s) * q_prev + s * q_next; // linear step
+			tg.push_back(q0);
+			clock += dt;
+		}
+		q_prev = q_next;    // next target becomes previous target
+		qd_prev = qd;
+	}
+
+	// add the final blend
+	qb = jtraj(q0, q_next, ArrayXd::LinSpaced(int(tacc2 / dt), 0, tacc2), qd_prev, qdf);
+	for (int ii = 0; ii < qb.rows() - 1; ii++) {
+		tg.push_back(qb.row(ii + 1));
+	}
+	R = ArrayXXd(tg.size(), tg[0].size());
+
+	for (int ii = 0; ii < tg.size(); ii++) {
+		R.row(ii) = tg[ii];
+	}
+
+	return R;
 }
